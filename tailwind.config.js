9594cd5847c69b82f304615c30a71ce3dc0bd127
yoshiaki44 <yo44ch@gmail.com>
2021-11-19 09:14:33 +0900

    bg-imageの追加、pluginの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "image-checkout": "url('/checkout.jpg')",
+    },},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide"),],
   mode: "jit",
 }
