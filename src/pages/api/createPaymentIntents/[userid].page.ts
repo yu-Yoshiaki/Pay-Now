5e6c34d9e92717460dbc4c27437f21c65d7c4565
yoshiaki44 <yo44ch@gmail.com>
2022-01-09 13:24:03 +0900

    catch error がエラーになるので、一時削除

--- src/pages/api/createPaymentIntents/[userid].page.ts
+++ src/pages/api/createPaymentIntents/[userid].page.ts
@@ -7,24 +7,17 @@ import { stripe } from "src/lib/stripe";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { userid } = req.query;
   if (req.method === "POST") {
-    try {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: 500,
-        currency: "jpy",
-        customer: userid as string,
-        payment_method: req.body,
-        off_session: true,
-        confirm: true,
-      });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 500,
+      currency: "jpy",
+      customer: userid as string,
+      payment_method: req.body,
+      off_session: true,
+      confirm: true,
+    });
 
-      res.status(200).json(paymentIntent);
-    } catch (err) {
-      // Error code will be authentication_required if authentication is needed
-      console.log("Error code is: ", err.code);
-      const paymentIntentRetrieved = await stripe.paymentIntents.retrieve(err.raw.payment_intent.id);
-      console.log("PI retrieved: ", paymentIntentRetrieved.id);
-    }
+    res.status(200).json(paymentIntent);
   }
 
-  res.status(200).json({ test: "test" });
+  res.status(200).json("Not existed GET Method");
 };
--- src/pages/api/setCard.page.ts
+++ src/pages/api/setCard.page.ts
@@ -4,15 +4,11 @@ import { stripe } from "src/lib/stripe";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    try {
-      const setupIntent = await stripe.setupIntents.create({
-        customer: req.body,
-        payment_method_types: ["card"],
-      });
-      res.status(200).json({ clientSecret: setupIntent.client_secret });
-    } catch (err) {
-      res.status(err.statusCode || 500).json(err.message);
-    }
-    res.status(200).json({ test: "test" });
+    const setupIntent = await stripe.setupIntents.create({
+      customer: req.body,
+      payment_method_types: ["card"],
+    });
+    res.status(200).json({ clientSecret: setupIntent.client_secret });
   }
+  res.status(200).json("Not existed GET Method");
 };
