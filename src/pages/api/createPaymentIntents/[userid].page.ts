b1d0a773eb8db0f9521609efdea73632254b79f9
yoshiaki44 <yo44ch@gmail.com>
2022-01-11 03:58:17 +0900

    説明文追加と、リターン変更

--- src/pages/api/createPaymentIntents/[userid].page.ts
+++ src/pages/api/createPaymentIntents/[userid].page.ts
@@ -1,3 +1,7 @@
+/*
+  Stripeで、新規支払い処理の作成＆実行
+*/
+
 /* eslint-disable no-console */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
@@ -8,7 +12,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { userid } = req.query;
   if (req.method === "POST") {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 500,
+      amount: 1300,
       currency: "jpy",
       customer: userid as string,
       payment_method: req.body,
@@ -16,7 +20,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       confirm: true,
     });
 
-    res.status(200).json(paymentIntent);
+    res.status(200).json({ message: "Complete Payment.", paymentIntent });
   }
 
   res.status(200).json("Not existed GET Method");
